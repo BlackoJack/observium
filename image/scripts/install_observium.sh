875888a583d1161dd186a73b69bb1e292eff7183
BlackoJack <admin@sysadminsk.ru>
2017-10-31 15:02:17 +0700

    Update install_observium.sh

--- image/scripts/install_observium.sh
+++ image/scripts/install_observium.sh
@@ -4,7 +4,10 @@ mkdir -p /opt/observium/{rrd,logs,mibs}
 cd /opt && wget http://www.observium.org/observium-community-latest.tar.gz && tar zxvf observium-community-latest.tar.gz && rm -f observium-community-latest.tar.gz
 mv /opt/observium/config.php.default /opt/observium/config.php
 cp /opt/observium/scripts/distro /usr/bin/distro
-mkdir -p /tmp/observium/{scripts,mibs} && mv /opt/observium/scripts/* /tmp/observium/scripts/ && mv /opt/observium/mibs/* /tmp/observium/mibs/
+mkdir -p /tmp/observium/{scripts,mibs,html}
+mv /opt/observium/scripts/* /tmp/observium/scripts/
+mv /opt/observium/mibs/* /tmp/observium/mibs/
+mv /opt/observium/html/* /tmp/observium/html/
 
 echo "33  */6   * * *   root    /opt/observium/discovery.php -h all >> /dev/null 2>&1" >> /etc/crontab
 echo "*/5 *     * * *   root    /opt/observium/discovery.php -h new >> /dev/null 2>&1" >> /etc/crontab
