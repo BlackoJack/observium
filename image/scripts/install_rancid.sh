9c1054cca660cfed40b06d208551a7637bd75f50
BlackoJack <admin@sysadminsk.ru>
2017-10-31 16:44:14 +0700

    Update install_rancid.sh

--- image/scripts/install_rancid.sh
+++ image/scripts/install_rancid.sh
@@ -34,6 +34,7 @@ echo "\$config['rancid_configs'][]              = \"/usr/local/rancid/var/observ
 echo "\$config['rancid_ignorecomments']        = 0;" >> /opt/observium/config.php
 echo "\$config['rancid_version'] = '3';" >> /opt/observium/config.php
 
+echo "0 4 * * * /usr/bin/php /opt/observium/scripts/generate-rancid.php > /usr/local/rancid//var/observium/router.db" >> /etc/crontab
 echo "0 5 * * * rancid /usr/local/rancid/bin/rancid-run" >> /etc/crontab
 echo "50 23 * * * rancid /usr/bin/find /usr/local/rancid/var/logs -type f -mtime   +2 -exec rm {} \;" >> /etc/crontab
 echo "" >> /etc/crontab
